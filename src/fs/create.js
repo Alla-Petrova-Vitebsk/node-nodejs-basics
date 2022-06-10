859067c21c00a645dd8d099704c74ba978a88ac2
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2022-06-10 12:32:02 +0300

    feat: task create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,14 @@
+import __dirname from "../dirName.js";
+import { join } from 'path';
+import { writeFile } from "fs/promises";
+
 export const create = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'fs','files', 'fresh.txt');
+  try {
+    await writeFile(pathToFile, 'TI am fresh and young',{flag:'wx'});
+  } catch (err) {
+    console.error('FS operation failed', err.message)
+  }
 };
+
+create();
